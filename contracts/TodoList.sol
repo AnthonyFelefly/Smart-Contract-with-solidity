5f49007361be33ed17134c4d8252ba6d973b07db
AnthonyFelefly <anthony.felefly@net.usj.edu.lb>
2022-08-21 16:14:53 +0300

    1. Project Setup

---
+++ contracts/TodoList.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Unidentified
+pragma solidity >=0.4.22 <0.9.0;
+
+contract TodoList
+{
+    uint public taskCount = 0;//state variable that is actually written to the blockchain
+
+}
